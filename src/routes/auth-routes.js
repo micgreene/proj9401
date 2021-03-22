0dfd123cc27b581a61da3c4889a8b765b9cf4649
micgreene <mgreene11111@yahoo.com>
2021-03-21 19:37:40 -0700

    finished scaff/server.js

---
+++ src/routes/auth-routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+
+const basicAuth = require('../auth/middleware/basic-auth-mw.js');
+const bearerAuth = require('../auth/middleware/bearer-auth-mw.js');
+const acl = require('../auth/middleware/acl-mw.js');
